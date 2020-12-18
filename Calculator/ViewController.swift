6cdacebddac37e38d9c8ab9ce8b77e01ca758441
ART_List <artbdarm9@mail.ru>
2020-12-18 13:07:51 +0300

    first commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Artem Listopadov on 12/18/20.
+//  Copyright © 2020 Artem Listopadov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
