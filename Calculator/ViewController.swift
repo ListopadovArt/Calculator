eb2e301709aec7236e9dca6e9395b05c4dee08cb
Artem Listopadov <artbdarm9@mail.ru>
2022-05-28 21:42:27 +0300

    init: start task

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    
+    
+    // MARK: - IBActions
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+    }
+}
+
